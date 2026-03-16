04b31cca2fa4c0f067397149fd6143c1268cc227
Staggered95 <shubhamsinghup720@gmail.com>
2026-03-16 19:19:54 +0530

    fixed player comp sizing

--- src/minicomps/MediaControllerIcons.jsx
+++ src/minicomps/MediaControllerIcons.jsx
@@ -97,7 +97,7 @@ const MediaControllers = ({ variant = 'bottomplayer', songId,  className = '' })
                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg>
                         ) : isPlaying ? (
-                            <svg className="w-8 h-8 sm:w-7 sm:h-7 drop-shadow-md" fill="currentColor" viewBox="0 0 24 24">
+                            <svg className="w-9 h-9 sm:w-7 sm:h-7 drop-shadow-md" fill="currentColor" viewBox="0 0 24 24">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z" />
                             </svg>
                         ) : (
