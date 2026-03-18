1bc5ab49286b47d710f476ae92a2eeb1f97faac8
Staggered95 <shubhamsinghup720@gmail.com>
2026-03-18 20:38:30 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -95,7 +95,6 @@ export default function Navbar() {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M21 21l-4.35-4.35m1.35-5.65a7 7 0 11-14 0 7 7 0 0114 0z"/>
                                 </svg>
                             </button>
-                            //theme toggle for my project
                             <div className="hidden md:block">
                                 <ThemeToggle/>
                             </div>
