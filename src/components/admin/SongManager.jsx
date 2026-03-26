82f687e39ba8cd02b7d8013b40b92f268e380390
Staggered95 <shubhamsinghup720@gmail.com>
2026-03-26 19:08:04 +0530

    minor changes

--- src/components/admin/SongManager.jsx
+++ src/components/admin/SongManager.jsx
@@ -161,7 +161,7 @@ const SongManager = ({ onAddNew, onEditSong, onSyncLyrics }) => {
                                                     <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" /></svg>
                                                 </button>
 
-                                                {/* 4. THE CONDITIONAL RENDER: Only Admins see the trash can */}
+                                                {/* 4. THE CONDITIONAL RENDER: Only Admins see the trash can  */}
                                                 {isAdmin && (
                                                     <button 
                                                         onClick={() => {
